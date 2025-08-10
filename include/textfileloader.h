b94c79e80f1cd982206db3450d341bf6637a502d
DietfridMali <karx11erx@googlemail.com>
2025-08-10 16:38:04 +0200

    fixed: removed duplicate and confliction declarations of app folders from GameItems added a few standard map definitions as text constants in playgrounds.cpp/h TextFileLoader can now also process text lines from a string buffer Added unified TextFileLoader::ReadString When the specified map couldn't be read, GameItems now loads a standard map as defined in playgrounds.cpp

--- include/textfileloader.h
+++ include/textfileloader.h
@@ -13,6 +13,10 @@ class TextFileLoader {
         typedef std::function<bool(String&)> tLineFilter; // Use std::function instead of raw function pointer
 
         TableDimensions ReadLines (const char * fileName, List<String>& fileLines, tLineFilter filter);
+
+        TableDimensions CopyLines(const String& lineBuffer, List<String>& textLines, tLineFilter filter);
+
+        TableDimensions ReadStream(std::istream& stream, List<String>& textLines, tLineFilter filter);
 };
 
 // =================================================================================================
--- src/textfileloader.cpp
+++ src/textfileloader.cpp
@@ -9,22 +9,33 @@
 
 // =================================================================================================
 
-TableDimensions TextFileLoader::ReadLines(const char * fileName, List<String>& fileLines, tLineFilter filter) {
-    std::ifstream f(fileName);
-    if (not f.is_open())
-        return -1;
+TableDimensions TextFileLoader::ReadLines(const char * fileName, List<String>& textLines, tLineFilter filter) {
+    std::ifstream stream(fileName);
+    if (not stream.is_open())
+        return TableDimensions(0,0);
+    return ReadStream(stream, textLines, filter);
+}
+
+
+TableDimensions TextFileLoader::CopyLines(const String& lineBuffer, List<String>& textLines, tLineFilter filter) {
+    std::istringstream stream(lineBuffer);
+    return ReadStream(stream, textLines, filter);
+}
+
+
+TableDimensions TextFileLoader::ReadStream(std::istream& stream, List<String>& textLines, tLineFilter filter) {
     std::string line;
     int rows = 0;
     int cols = 0;
-    while (std::getline(f, line)) {
+    while (std::getline(stream, line)) {
         String s(line);
         if (filter(s)) {
             rows++;
             cols = std::max(cols, int(line.length()));
-            fileLines.Append(std::move(s));
+            textLines.Append(std::move(s));
         }
     }
-    return TableDimensions (cols, rows);
+    return TableDimensions(cols, rows);
 }
 
 // =================================================================================================
